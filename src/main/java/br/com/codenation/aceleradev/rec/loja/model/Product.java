04967ba1926823bf6efa496b3c20642ec9b605ef
bjcc@cin.ufpe.br <bjcc@cin.ufpe.br>
2019-02-02 13:33:06 -0300

    Finish model

--- src/main/java/br/com/codenation/aceleradev/rec/loja/model/Product.java
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/model/Product.java
@@ -2,30 +2,18 @@ package br.com.codenation.aceleradev.rec.loja.model;
 
 import java.util.Date;
 
-public class Product {
+public abstract class Product {
 
     private Long id;
     private double price;
     private int stockCount;
     private int category;
-    private String nameArtist;
-    private int musicCount;
-    private String nameDirector;
-    private String initYear;
-    private String version;
-    private String requisits;
 
     public Product(Long id, double price, int stockCount, int category, String nameArtist, int musicCount, String nameDirector, String initYear, String version, String requisits) {
         this.id = id;
         this.price = price;
         this.stockCount = stockCount;
         this.category = category;
-        this.nameArtist = nameArtist;
-        this.musicCount = musicCount;
-        this.nameDirector = nameDirector;
-        this.initYear = initYear;
-        this.version = version;
-        this.requisits = requisits;
     }
 
     public Product() {
@@ -63,52 +51,5 @@ public class Product {
         this.category = category;
     }
 
-    public String getNameArtist() {
-        return nameArtist;
-    }
-
-    public void setNameArtist(String nameArtist) {
-        this.nameArtist = nameArtist;
-    }
-
-    public int getMusicCount() {
-        return musicCount;
-    }
-
-    public void setMusicCount(int musicCount) {
-        this.musicCount = musicCount;
-    }
-
-    public String getNameDirector() {
-        return nameDirector;
-    }
-
-    public void setNameDirector(String nameDirector) {
-        this.nameDirector = nameDirector;
-    }
-
-    public String getInitYear() {
-        return initYear;
-    }
-
-    public void setInitYear(String initYear) {
-        this.initYear = initYear;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getRequisits() {
-        return requisits;
-    }
-
-    public void setRequisits(String requisits) {
-        this.requisits = requisits;
-    }
 
 }
---
+++ src/main/java/br/com/codenation/aceleradev/rec/loja/model/Show.java
@@ -0,0 +1,17 @@
+package br.com.codenation.aceleradev.rec.loja.model;
+
+public class Show extends  Product{
+
+    private String artistName;
+    private int countMusics;
+
+    public Show(Long id, double price, int stockCount, int category, String nameArtist, int musicCount, String nameDirector, String initYear, String version, String requisits, String artistName, int countMusics) {
+        super(id, price, stockCount, category, nameArtist, musicCount, nameDirector, initYear, version, requisits);
+        this.artistName = artistName;
+        this.countMusics = countMusics;
+    }
+
+    public Show() {
+    }
+
+}
